714ccb3d69babc7190790575384873d4d8969c68
colbygatte <colbygatte@gmail.com>
2018-03-05 16:07:42 -0600

    Refactor test

--- tests/UnitTests/ColumnGroupingTest.php
+++ tests/UnitTests/ColumnGroupingTest.php
@@ -15,14 +15,14 @@ class ColumnGroupingTest extends TestCase
         $header = new Header(['spec1', 'val1', 'spec2', 'val2']);
         $header->makeGroup('specs', ['spec', 'val']);
 
-        $csv = new Csv($header);
-
-        $row = $csv->append(['length', '10', 'width', '20']);
+        $specs = (new Csv($header))
+            ->append(['length', '10', 'width', '20'])
+            ->getGroup('specs');
 
         $this->assertEquals([
             ['spec' => 'length', 'val' => '10'],
             ['spec' => 'width', 'val' => '20']
-        ], $row->getGroup('specs'));
+        ], $specs);
     }
 
     /** @test */
