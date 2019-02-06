e1ae8ee32f57fb4f0d2470dd9145468248f53a65
Colby <colbygatte@gmail.com>
2019-02-06 14:57:40 -0600

    Fix helper functions, remove CSV_FUNCTIONS constant

--- utilities/helpers.php
+++ utilities/helpers.php
@@ -1,31 +1,25 @@
 <?php
 
-use ColbyGatte\SmartCsv\Csv;
 use ColbyGatte\SmartCsv\CsvUtils;
-use ColbyGatte\SmartCsv\Header;
 use ColbyGatte\SmartCsv\Iterators\Sip;
 
-if (! defined('CSV_FUNCTIONS') || ! CSV_FUNCTIONS) {
-    return;
-}
-
 if (! function_exists('csv_sip')) {
-    function csv_sip($file_path)
+    function csv_sip($file)
     {
-        return new Sip($file_path);
+        return new Sip($file);
     }
 }
 
 if (! function_exists('csv_alter')) {
-    function csv_alter($original_file_path, $altered_file_path, $callback)
+    function csv_alter($original, $altered, $callback)
     {
-        CsvUtils::alter($original_file_path, $altered_file_path, $callback);
+        CsvUtils::alter($original, $altered, $callback);
     }
 }
 
 if (! function_exists('csv_slurp')) {
-    function csv_slurp($file_path)
+    function csv_slurp($file)
     {
-        return CsvUtils::slurp($file_path);
+        return CsvUtils::slurp($file);
     }
 }
